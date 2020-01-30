90c8c66f051edff319ad24760c8ad22fe4c2331f
Salman Zafar <salman.zafar@innowi.com>
2020-01-30 12:46:35 +0500

    inital setup

---
+++ src/ApiExceptionHandlerServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salman
+ * Date: 01/06/2020
+ * Time: 1:34 PM
+ */
+
+namespace Salman\ApiExceptionHandler;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiExceptionHandlerServiceProvider extends ServiceProvider
+{
+
+}
